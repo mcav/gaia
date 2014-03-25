752e3718bb6f79b46e5700849babae8d4cb50203
Marcus Cavanaugh <m@mcav.com>
2014-03-25 09:07:03 -0700

    Bug 986753 - [Tarako] Alarm Clock can get killed in the background and never ring.

--- apps/clock/js/require_config.js
+++ apps/clock/js/require_config.js
@@ -1,4 +1,13 @@
 requirejs.config({
+  // Set waitSeconds to a high value. Under high memory pressure on
+  // low-memory devices, it may take a long time for the modules to
+  // load. For instance, when an alarm rings, we don't want to time
+  // out trying to bring up the notification screen, even if it takes
+  // a minute or two to load the clock app (because they're editing a
+  // photo or some other high-memory activity). In production, modules
+  // aren't going to disappear, so it makes sense to set an
+  // arbitrarily high value here.
+  waitSeconds: 180,
   paths: {
     shared: '../shared'
   },
